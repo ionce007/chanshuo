c77ce7f6d2c4f0c6435b0e8b1e560b82f6e11a4e
ionce007 <ionce@163.com>
2026-06-09 23:32:31 +0800

    修复cann't GET错误

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(flash());
     var ips = ipArr.filter(item => item && regex.test(item));
     var ipValue = ips && ips.length > 0 ? ips[0] : (new Date()).getTime();
     var deviceId = crypto.createHash('md5').update(ipValue.toString()).digest('hex');
-    res.json({deviceId: deviceId });
+    res.json({ deviceId: deviceId });
   });
   app.use(
     serveStatic(path.join(__dirname, 'data', 'index'), serveStaticOptions)
@@ -183,3 +183,5 @@ server.on('listening', () => {
 });
 
 module.exports = app;
+
+
